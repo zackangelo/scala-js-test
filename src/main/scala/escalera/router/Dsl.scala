d84346dca01aa0f3ff984f692644dee2801035ac
Zack Angelo <zackangelo@gmail.com>
2015-01-11 21:03:23 -0400

    Refactor route dsl into routing package

--- src/main/scala/escalera/router/Dsl.scala
+++ src/main/scala/escalera/router/Dsl.scala
@@ -1,25 +1,70 @@
 package escalera.router
 
 import shapeless._
-import shapeless.ops.function._
+import shapeless.ops.function.FnToProduct
 
-trait RouteHandler {
-  def apply(children: => Seq[RouteMapping[_]]) = ???
+trait RouteHandler
+
+trait RoutePattern[L <: HList] {
+  val pattern: PathMatcher[L]
+  def ->[R0 <: RouteHandler](rh: => R0): RouteDef[L, R0]
+  def ->[Fn0, R0 <: RouteHandler](f: Fn0)
+                                 (implicit ftp: FnToProduct.Aux[Fn0, (L) => R0]): RouteDef[L, R0]
 }
 
-trait RouteMapping[F] {
-  val path:PathMatcher[_]
-  //    val handler:RouteHandler
-  def ->(f:F) = ???
+case class RouteMatchResult(handler: RouteHandler, childMatcher: Option[RouteMatcher], remainingPath: String)
+
+trait RouteMatcher extends PartialFunction[String, RouteMatchResult] {
+  def or(next: RouteMatcher): RouteMatcher = new OrRouteMatcher(this, next)
 }
 
-object RouteMapping {
-  implicit def pm2mapping[F,H <: HList](m:PathMatcher[H])
-                                       (implicit ftp: FnToProduct.Aux[F, H => RouteHandler]):RouteMapping[F] = {
-    new RouteMapping[F] {
-      val path = m
-      override def ->(f:F) = ???
+class OrRouteMatcher(a: RouteMatcher, b: RouteMatcher) extends RouteMatcher {
+  override def isDefinedAt(x: String): Boolean = a.isDefinedAt(x) || b.isDefinedAt(x)
+  override def apply(v1: String): RouteMatchResult = a.applyOrElse(v1, b)
+}
+
+trait RouteDef[L <: HList, R <: RouteHandler] extends RouteMatcher { self =>
+  val pattern: PathMatcher[L]
+
+  def handler(segments: L): R
+
+  def apply(path: String) = pattern(path) match {
+    case Matched(segments, rest) => {
+      val h = handler(segments)
+      RouteMatchResult(h, children(h), rest)
     }
+    case _ => throw new IllegalStateException(s"Matcher is invalid for path $path")
+  }
+
+  def isDefinedAt(path: String) = pattern.apply(path) match {
+    case Matched(_, _) => true
+    case _ => false
+  }
+
+  def children(rh: R): Option[RouteMatcher] = None
+
+  def then(childFn: R => RouteMatcher) = new RouteDef[L, R] {
+    val pattern = self.pattern
+    def handler(segments: L) = self.handler(segments)
+    override def children(rh: R) = Some(childFn(rh))
+  }
+}
+
+object route {
+  def apply[L0 <: HList](pmatcher: PathMatcher[L0]) = new RoutePattern[L0] {
+    val pattern: PathMatcher[L0] = pmatcher
+
+    def ->[R0 <: RouteHandler](rh: => R0): RouteDef[L0, R0] = new RouteDef[L0, R0] {
+      val pattern = pmatcher
+      def handler(segments: L0): R0 = rh
+    }
+
+    def ->[Fn0, R0 <: RouteHandler](f: Fn0)
+                                   (implicit ftp: FnToProduct.Aux[Fn0, (L0) => R0]) =
+      new RouteDef[L0, R0] {
+        val pattern: PathMatcher[L0] = pmatcher
+        def handler(segments: L0): R0 = ftp(f)(segments)
+      }
   }
 }
 
--- src/main/scala/myapp/HackerNews.scala
+++ src/main/scala/myapp/HackerNews.scala
@@ -13,9 +13,8 @@ import org.scalajs.dom.window
  */
 @JSExport
 object HackerNews {
-
-  import RouteMapping._
   import PathMatcher._
+  import escalera.router.route
 
   case class StoriesRoute() extends RouteHandler
   case class StoryRoute(storyId: String) extends RouteHandler
@@ -26,90 +25,17 @@ object HackerNews {
   case class UsersRoute() extends RouteHandler
   case class UserRoute(userId: String) extends RouteHandler
 
-  import shapeless._
-  import shapeless.ops.function.FnToProduct
-
-  trait RoutePattern[L <: HList] {
-    val pattern: PathMatcher[L]
-    def ->[R0 <: RouteHandler](rh: => R0): RouteDef[L, R0]
-    def ->[Fn0, R0 <: RouteHandler](f: Fn0)
-                                   (implicit ftp: FnToProduct.Aux[Fn0, (L) => R0]): RouteDef[L, R0]
-  }
-
-  case class RouteMatchResult(handler: RouteHandler, childMatcher: Option[RouteMatcher], remainingPath: String)
-
-  trait RouteMatcher extends PartialFunction[String, RouteMatchResult] {
-    def or(next: RouteMatcher): RouteMatcher = new OrRouteMatcher(this, next)
-  }
-
-  class OrRouteMatcher(a: RouteMatcher, b: RouteMatcher) extends RouteMatcher {
-    override def isDefinedAt(x: String): Boolean = a.isDefinedAt(x) || b.isDefinedAt(x)
-    override def apply(v1: String): RouteMatchResult = a.applyOrElse(v1, b)
-  }
-
-  trait RouteDef[L <: HList, R <: RouteHandler] extends RouteMatcher {
-    self =>
-    val pattern: PathMatcher[L]
-
-    def handler(segments: L): R
-
-    def apply(path: String) = pattern(path) match {
-      case Matched(segments, rest) => {
-        window.console.log(s"path = $path, segments = $segments, rest = $rest")
-
-        val h = handler(segments)
-        RouteMatchResult(h, children(h), rest)
-      }
-      case _ => throw new IllegalStateException(s"Matcher is invalid for path $path")
-    }
-
-    def isDefinedAt(path: String) = pattern.apply(path) match {
-      case Matched(_, _) => true
-      case _ => false
-    }
-
-    def children(rh: R): Option[RouteMatcher] = None
-
-    def then[RH <: RouteHandler](childFn: R => RouteMatcher) = new RouteDef[L, R] {
-      val pattern = self.pattern
-      def handler(segments: L) = self.handler(segments)
-      override def children(rh: R) = Some(childFn(rh))
-    }
-  }
-
-  object route2 {
-    def apply[L0 <: HList](pmatcher: PathMatcher[L0]) = new RoutePattern[L0] {
-      val pattern: PathMatcher[L0] = pmatcher
-
-      def ->[R0 <: RouteHandler](rh: => R0): RouteDef[L0, R0] = new RouteDef[L0, R0] {
-        val pattern = pmatcher
-        def handler(segments: L0): R0 = rh
-      }
-
-      def ->[Fn0, R0 <: RouteHandler](f: Fn0)
-                                     (implicit ftp: FnToProduct.Aux[Fn0, (L0) => R0]) =
-        new RouteDef[L0, R0] {
-          val pattern: PathMatcher[L0] = pmatcher
-          def handler(segments: L0): R0 = ftp(f)(segments)
-        }
-    }
-  }
-
-  val router3 = route2("users") -> UsersRoute() then { users =>
-    route2(Segment) -> (UserRoute(_))
-  }
-
   @JSExport
   val router2 =
-    route2("stories") -> StoriesRoute() then { stories =>
-      route2(Segment) -> (StoryRoute(_)) then { story =>
-        route2("comments") -> StoryCommentsRoute(story.storyId) then { comments =>
-          route2(Segment) -> StoryCommentRoute()
+    route("stories") -> StoriesRoute() then { stories =>
+      route(Segment) -> (StoryRoute(_)) then { story =>
+        route("comments") -> StoryCommentsRoute(story.storyId) then { comments =>
+          route(Segment) -> StoryCommentRoute()
         } or
-        route2("description") -> StoryDescriptionRoute(story.storyId)
+        route("description") -> StoryDescriptionRoute(story.storyId)
       }
-    } or (route2("users") -> UsersRoute() then { users =>
-      route2(Segment) -> (UserRoute(_))
+    } or (route("users") -> UsersRoute() then { users =>
+      route(Segment) -> (UserRoute(_))
     })
 
 
@@ -122,6 +48,8 @@ object HackerNews {
           applyRouter(remain, child, handler :: matched)
         case Some(RouteMatchResult(handler, None, remain)) if remain.isEmpty =>
           handler :: matched
+        case _ if path.isEmpty =>
+          matched
         case _ =>
           List.empty
       }
